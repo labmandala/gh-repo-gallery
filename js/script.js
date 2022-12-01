224482bbcc2190019bfc159cbe95b540434573c2
sarah <skg@neofilm.us>
2022-12-01 14:58:59 -0500

    Fetch API JSON data

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 // This div is where profile information will appear
 const overview = document.querySelector(".overview");
 const username = "labmandala";
+
+const gitUserInfo = async function () {
+  const userInfo = await fetch(`https://api.github.com/users/${username}`);
+  const data = await userInfo.json();
+};
+
+gitUserInfo();
