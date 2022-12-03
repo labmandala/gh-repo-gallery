10c6b6edfeb82a34d15d012eb85d5c395b5ccd85
sarah <skg@neofilm.us>
2022-12-03 18:19:42 -0500

    Fetch & display user information

--- js/script.js
+++ js/script.js
@@ -5,6 +5,26 @@ const username = "labmandala";
 const gitUserInfo = async function () {
   const userInfo = await fetch(`https://api.github.com/users/${username}`);
   const data = await userInfo.json();
+  // Log out the response to the console, then call fx to see results
+  //console.log(data);
+  displayUserInfo(data);
 };
 
 gitUserInfo();
+
+const displayUserInfo = function (data) {
+  const div = document.createElement("div");
+  div.classList.add("user-info");
+  div.innerHTML = `
+      <figure>
+        <img alt="user avatar" src=${data.avatar_url} />
+      </figure>
+      <div>
+        <p><strong>Name:</strong> ${data.name}</p>
+        <p><strong>Bio:</strong> ${data.bio}</p>
+        <p><strong>Location:</strong> ${data.location}</p>
+        <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+      </div>
+    `;
+  overview.append(div);
+};
