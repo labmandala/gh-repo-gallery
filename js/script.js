1b8253324d5480f6b98d952274c3e7bd5910dd7b
sarah <skg@neofilm.us>
2022-12-04 10:20:21 -0500

    Select the repos list

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // This div is where profile information will appear
 const overview = document.querySelector(".overview");
 const username = "labmandala";
+const repoList = document.querySelector(".repo-list");
 
 const gitUserInfo = async function () {
   const userInfo = await fetch(`https://api.github.com/users/${username}`);
