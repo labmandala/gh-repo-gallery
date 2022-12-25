6d9a1ec74dc0d8e8f222b13023b8cdbfef60dc18
sarah <skg@neofilm.us>
2022-12-25 08:40:53 -0500

    Test dynamic search, tidy code & clean comments

--- js/script.js
+++ js/script.js
@@ -1,23 +1,19 @@
-const overview = document.querySelector(".overview"); // Div where profile info appears
+const overview = document.querySelector(".overview");
 const username = "labmandala";
-const repoList = document.querySelector(".repo-list"); // Select ul to display repos list
-const allReposContainer = document.querySelector(".repos"); // Select section where all repo info appears
-const repoData = document.querySelector(".repo-data"); // Select section where individual repo data will appear
-const viewReposButton = document.querySelector(".view-repos"); // Select the Back to Repo Gallery button
-const filterInput = document.querySelector(".filter-repos"); // Select the input with the “Search by name” placeholder
+const repoList = document.querySelector(".repo-list");
+const allReposContainer = document.querySelector(".repos");
+const repoData = document.querySelector(".repo-data");
+const viewReposButton = document.querySelector(".view-repos");
+const filterInput = document.querySelector(".filter-repos");
 
-// Fetch data from GitHub API, pull GH profile data
 const gitUserInfo = async function () {
   const userInfo = await fetch(`https://api.github.com/users/${username}`);
   const data = await userInfo.json();
-  // Log out the response to the console, then call fx to see results
-  //console.log(data);
   displayUserInfo(data);
 };
 
 gitUserInfo();
 
-// Pull additional info from user profile & display on web page
 const displayUserInfo = function (data) {
   const div = document.createElement("div");
   div.classList.add("user-info");
@@ -36,16 +32,14 @@ const displayUserInfo = function (data) {
   gitRepos();
 };
 
-// Fetch repo data using endpoint & parameters according to API documentation
 const gitRepos = async function () {
-// Parameters to sort repos by most recently updated first & show up to 100 repos per page 
-  const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const fetchRepos = await fetch(
+    `https://api.github.com/users/${username}/repos?sort=updated&per_page=100`
+  );
   const repoData = await fetchRepos.json();
-  //console.log(gitRepos);
   displayRepos(repoData);
 };
 
-// Display repo’s info using properties from fetched data
 const displayRepos = function (repos) {
   filterInput.classList.remove("hide");
   for (const repo of repos) {
@@ -56,7 +50,6 @@ const displayRepos = function (repos) {
   }
 };
 
-// Event listener for repo title to show the individual repo info
 repoList.addEventListener("click", function (e) {
   if (e.target.matches("h3")) {
     const repoName = e.target.innerText;
@@ -64,29 +57,27 @@ repoList.addEventListener("click", function (e) {
   }
 });
 
-// Get specific repo info
 const getRepoInfo = async function (repoName) {
-  const fetchInfo = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
+  const fetchInfo = await fetch(
+    `https://api.github.com/repos/${username}/${repoName}`
+  );
   const repoInfo = await fetchInfo.json();
   console.log(repoInfo);
-  // Fetch data from language_url property of repoInfo
   const fetchLanguages = await fetch(repoInfo.languages_url);
   const languageData = await fetchLanguages.json();
-  console.log(languageData); // Return data on the languages used in the repo
+  console.log(languageData);
 
-  // Make a list of languages
   const languages = [];
   for (const language in languageData) {
-   languages.push(language);
+    languages.push(language);
   }
 
-  displayRepoInfo(repoInfo, languages); // Call fx to display repo info 
+  displayRepoInfo(repoInfo, languages);
 };
 
-// Display specific, individual repo info
 const displayRepoInfo = function (repoInfo, languages) {
-  viewReposButton.classList.remove("hide"); // Displays Back to Repo Gallery button
-  repoData.innerHTML = ""; // Empty HTML where individual repo data will appear
+  viewReposButton.classList.remove("hide");
+  repoData.innerHTML = "";
   repoData.classList.remove("hide");
   allReposContainer.classList.add("hide");
   const div = document.createElement("div");
@@ -95,12 +86,13 @@ const displayRepoInfo = function (repoInfo, languages) {
     <p>Description: ${repoInfo.description}</p>
     <p>Default Branch: ${repoInfo.default_branch}</p>
     <p>Languages: ${languages.join(", ")}</p>
-    <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
+    <a class="visit" href="${
+      repoInfo.html_url
+    }" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
   `;
   repoData.append(div);
 };
 
-// Event listener for the Back to Repo Gallery
 viewReposButton.addEventListener("click", function () {
   allReposContainer.classList.remove("hide");
   repoData.classList.add("hide");
@@ -109,14 +101,9 @@ viewReposButton.addEventListener("click", function () {
 
 filterInput.addEventListener("input", function (e) {
   const searchText = e.target.value;
-  //console.log(searchText);
-  // Select all elements on page with repo class
   const repos = document.querySelectorAll(".repo");
   const searchLowerText = searchText.toLowerCase();
 
-  // Loop through each repo in element
-  // If the repo contains the text, show
-  // If it doesn’t contain the text, hide repo
   for (const repo of repos) {
     const repoLowerText = repo.innerText.toLowerCase();
     if (repoLowerText.includes(searchLowerText)) {
